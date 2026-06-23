bdf69a81253f2653524e194810f301eb938ab3c0
nellanap21 <ap543@student.london.ac.uk>
2026-06-22 17:15:27 -0700

    update run_genome.py

--- run_genome.py
+++ run_genome.py
@@ -6,52 +6,61 @@ import pybullet_data as pd
 import time
 import creature
 import genome as genlib
+import sys
+import os
 
-# NOTE: change so that it can take a csv_file 
-p.connect(p.GUI)
-p.setPhysicsEngineParameter(enableFileCaching=0)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+def main(csv_file):
+    assert os.path.exists(csv_file), "Tried to load " + csv_file + "but it does not exist"
+    
+    p.connect(p.GUI)
+    p.setPhysicsEngineParameter(enableFileCaching=0)
+    p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
 
-# add floor
-plane_shape = p.createCollisionShape(p.GEOM_PLANE)
-floor = p.createMultiBody(plane_shape, plane_shape)
+    # add floor
+    plane_shape = p.createCollisionShape(p.GEOM_PLANE)
+    floor = p.createMultiBody(plane_shape, plane_shape)
 
-# set gravity
-p.setGravity(0, 0, -10)
+    # set gravity
+    p.setGravity(0, 0, -10)
 
-c = creature.Creature(gene_count = 5)
+    c = creature.Creature(gene_count = 5)
 
-dna = genlib.Genome.from_csv('9_elite.csv')
-c.set_dna(dna)
+    dna = genlib.Genome.from_csv(csv_file)
+    c.set_dna(dna)
 
-# save it to XML
-with open('test.urdf', 'w') as f:
-    c.get_expanded_links()
-    f.write(c.to_xml())
-# load it into the sim
-cid = p.loadURDF('test.urdf')
+    # save it to XML
+    with open('test.urdf', 'w') as f:
+        c.get_expanded_links()
+        f.write(c.to_xml())
+    # load it into the sim
+    cid = p.loadURDF('test.urdf')
 
-# assume creature sitting at origin
-c.update_position([0,0,0])
+    # assume creature sitting at origin
+    c.update_position([0,0,0])
 
-# sets position to slightly above the ground to fix the flying problem
-p.resetBasePositionAndOrientation(cid, [0,0,3], [0,0,0,1])
+    # sets position to slightly above the ground to fix the flying problem
+    p.resetBasePositionAndOrientation(cid, [0,0,3], [0,0,0,1])
 
-# needed for mac users to interact
-while True:
-    for jid in range(p.getNumJoints(cid)):
-        m = c.get_motors()[jid]
+    # needed for mac users to interact
+    while True:
+        for jid in range(p.getNumJoints(cid)):
+            m = c.get_motors()[jid]
 
-        p.setJointMotorControl2(cid, jid, 
-                                controlMode=p.VELOCITY_CONTROL,
-                                targetVelocity=m.get_output(),
-                                force = 5)
+            p.setJointMotorControl2(cid, jid, 
+                                    controlMode=p.VELOCITY_CONTROL,
+                                    targetVelocity=m.get_output(),
+                                    force = 5)
 
-    pos, orn = p.getBasePositionAndOrientation(cid)
-    c.update_position(pos)
-    #print(c.get_distance_travelled())
-    p.resetDebugVisualizerCamera(5, 0, 200, pos)
+        pos, orn = p.getBasePositionAndOrientation(cid)
+        c.update_position(pos)
+        #print(c.get_distance_travelled())
+        p.resetDebugVisualizerCamera(5, 0, 200, pos)
 
-    p.stepSimulation()
-    # time.sleep(0.1) # 10 times a second
-    time.sleep(1.0/240) # pybullet is 240 frames per second
+        p.stepSimulation()
+        # time.sleep(0.1) # 10 times a second
+        time.sleep(1.0/240) # pybullet is 240 frames per second
+    
+
+if __name__ == "__main__":
+    assert len(sys.argv) == 2, "Usage: python run_genome.py csv_filename"
+    main(sys.argv[1])
